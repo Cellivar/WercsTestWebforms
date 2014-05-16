55760c7b5dfd0cc6fc2e405e3005f5f6fbbe53af
Cliff Chapman <chapman.cliff@gmail.com>
2014-05-16 18:04:24 -0400

    Added Linq DAL layer

---
+++ WercsTestWebForms/DAL/LanguagesDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace WercsTestWebForms.DAL
+{
+    public static class LanguagesDAL
+    {
+        /// <summary>
+        /// Get a number of language objects from the DB
+        /// </summary>
+        /// <param name="number">Number of languages to grab</param>
+        /// <returns>Array of language objects</returns>
+        public static Array GetLanguages(int number)
+        {
+            return new LocalDBDataContext().T_Languages.Take(number).ToArray();
+        }
+    }
+}
---
+++ WercsTestWebForms/DAL/LanguagesXMLDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WercsTestWebForms.DAL
+{
+    public static class LanguagesXMLDAL
+    {
+        /// <summary>
+        /// Return all names of languages in the DB
+        /// </summary>
+        /// <returns>Array of language names.</returns>
+        public static Array GetAllNames()
+        {
+            var langfile = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/LocalDBXML.xml"));
+            // For giggles, let's do chaining syntax instead of SQL this time.
+            var retList = langfile.Root.Elements().Select(lang => lang.Attribute("F_Language_Name").Value);
+            return retList.ToArray();
+        }
+    }
+}
